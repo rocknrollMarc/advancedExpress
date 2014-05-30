528d110003f553312db0e87585fe33c53b2b2e9f
Marc Bluemner <Marc.Bluemner@mspag.com>
2014-05-30 10:33:54 +0200

    updated

--- test/heartbeat.js
+++ test/heartbeat.js
@@ -1,13 +1,20 @@
 var app = require('../app')
 , request = require('supertest');
 
-describe('vision heartbeat api', function() {
-  describe('when requesting resource /heartbeat', function() {
-    it('should respond with 200', function(done) {
+describe('vision', function(){
+  describe('when requesting resource /heartbeat', function(){
+    it('should respond with 200', function(done){
       request(app)
-      .get('heartbeat')
-      .expect('Content-Type', /json/)
-      .epect(200, done);
+        .get('/heartbeat')
+        .expect(200, done);
     });
+  });
+
+  describe('when requesting resource /missing', function(){
+    it('should respond with 404', function(done){
+      request(app)
+        .get('/missing')
+        .expect(404, done);
+    })
   });
 });
